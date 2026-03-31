338c04880203068f6b22ae10aa9fd5cecafe7ae5
Alexander Saw <sawsasha26@gmail.com>
2026-03-31 11:19:16 +0000

    fix: increase JSON body limit to 50mb for image uploads

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ function redactObject(obj) {
   return result;
 }
 
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // --- Auto-logging middleware for all mutations ---
